f3e70fe63fe89c2a1099bac65c08ed02deb7b493
JoaoPauloPereirax <joaopaulop.dev@gmail.com>
2024-08-17 19:32:19 -0300

    Correction db conection

--- src/Config.php
+++ src/Config.php
@@ -6,7 +6,7 @@ class Config {
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
-    const DB_DATABASE = 'test.test';
+    const DB_DATABASE = 'test';
     CONST DB_USER = 'root';
     const DB_PASS = '';
 
