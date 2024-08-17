0eecfc1579d2a0c108330ae3acfb9344ce576b68
JoaoPauloPereirax <joaopaulop.dev@gmail.com>
2024-08-17 17:45:46 -0300

    connection of header and home

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -1,1 +1,11 @@
-Olá mundãoooo
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Lista</title>
+</head>
+<body>
+       <?php $render('header'); ?>
+</body>
+</html>
